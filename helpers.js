c623bd9245922792d6cf7cda60a2c026d83c7bc9
potatolight <shuguangbai@gmail.com>
2020-08-10 03:24:35 +0000

    add helper

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+//compare email with in the database//
 const getUserByEmail = function(email, database) {
   let user = "";
   for (let key in database) {
@@ -9,4 +10,24 @@ const getUserByEmail = function(email, database) {
   return undefined;
 };
 
-module.exports = { getUserByEmail };
+//get value of the object which key is eqaul to id
+const urlsForUser = function(id, urlDatabase) {
+  let output = {};
+  for (let key in urlDatabase) {
+    if (urlDatabase[key]["userID"] === id) {
+      output[key] = urlDatabase[key];
+    }
+  }
+  return output;
+};
+//generate random string//
+const generateRandomString = function() {
+  const string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let output = '';
+  for (let i = 0; i < 6; i++) {
+    output += string[Math.floor(Math.random() * string.length)];
+  }
+  return output;
+};
+
+module.exports = { getUserByEmail, urlsForUser,  generateRandomString};
